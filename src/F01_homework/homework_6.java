f19aaaf5935b0d02823dab57ea208f787daa2216
Dương Minh Thông <duongminhthong1608@gmail.com>
2024-10-10 18:23:31 +0700

    update homework6: ex3,5

--- src/F01_homework/homework_6.java
+++ src/F01_homework/homework_6.java
@@ -71,7 +71,32 @@ public class homework_6 {
 
     static class homework3 {
         public static void main(String[] args) {
+            int[] array1 = {1, 3, 5, 7};
+            int[] array2 = {2, 4, 6, 8, 10};
+            int[] mergedArray = mergeArrays(array1, array2);
+            System.out.println("Array after mixed: " + Arrays.toString(mergedArray));
+        }
+        public static int[] mergeArrays(int[] array1, int[] array2) {
+            int n1 = array1.length;
+            int n2 = array2.length;
+            int[] newArr = new int[n1 + n2];
 
+            int i = 0, j = 0, k = 0;
+
+            while (i < n1 && j < n2) {
+                if (array1[i] <= array2[j]) {
+                    newArr[k++] = array1[i++];
+                } else {
+                    newArr[k++] = array2[j++];
+                }
+            }
+            while (i < n1) {
+                newArr[k++] = array1[i++];
+            }
+            while (j < n2) {
+                newArr[k++] = array2[j++];
+            }
+            return newArr;
         }
     }
 
@@ -105,6 +130,46 @@ public class homework_6 {
                     System.out.println(a[k - 1]);
                 }
             }
+        }
+    }
+
+    static class homework5 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            int count25 = 0, count50 = 0;
+            for (int i = 0; i < n; i++) {
+                int bill = scanner.nextInt();
+                switch (bill) {
+                    case 25:
+                        count25++;
+                        break;
+                    case 50:
+                        if (count25 > 0) {
+                            count25--;
+                            count50++;
+                        } else {
+                            System.out.println("NO");
+                            return;
+                        }
+                        break;
+                    case 100:
+                        if (count50 > 0 && count25 > 0) {
+                            count50--;
+                            count25--;
+                        } else if (count25 >= 3) {
+                            count25 -= 3;
+                        } else {
+                            System.out.println("NO");
+                            return;
+                        }
+                        break;
+                    default:
+                        System.out.println("NO");
+                        return;
+                }
+            }
+            System.out.println("YES");
         }
     }
 
